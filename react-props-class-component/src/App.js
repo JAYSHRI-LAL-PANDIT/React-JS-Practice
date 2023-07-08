29b4d6b4ddc76d4b23898234d348c64eb75c519a
JAYSHRI LAL PANDIT <102451566+JAYSHRI-LAL-PANDIT@users.noreply.github.com>
2023-07-08 16:28:01 +0530

    Add files via upload

---
+++ react-props-class-component/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './Student'
+import React from 'react'
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    this.state={
+      name:"Anil"
+    }
+  }
+  render()
+  {
+    return (
+      <div className="App">
+       <h1>Props !</h1>
+       <Student name={this.state.name} email="anil@test.com"></Student>
+       <button onClick={()=>this.setState({name:"Sidhu"})} >Update Name</button>
+      </div>
+    );
+  }
+}
+
+export default App;
